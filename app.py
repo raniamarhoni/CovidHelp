6090443f7d1e5e80c8d108b439520f5c75304555
raniamarhoni <marhoni@outlook.com>
2020-11-15 13:44:31 +0000

    Connected Mongodb database

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-import os 
+import os
+import pymongo
 from flask import Flask
+from flask_pymongo import flask_pymongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+
+if os.path.exists("env.py"):
+    import env
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "covidhelp"
+COLLECTION = "users"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MONGODB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+documents = coll.find()
+
+
+for doc in documents:
+    print(doc)
 
 
 @app.route('/')
