bf8913a879956318fada5676e5a06ebd6c8aa369
raniamarhoni <marhoni@outlook.com>
2020-11-29 12:46:30 +0000

    Register Page added

--- app.py
+++ app.py
@@ -23,6 +23,44 @@ mongo = PyMongo(app)
 def home():
     return render_template("home.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        # check if email already exists in db
+        existing_email = mongo.db.users.find_one(
+            {"email": request.form.get("email")})
+
+        if existing_email:
+            flash("Email already exists")
+            return redirect(url_for("register"))
+
+        # check if the passwords match
+        if request.form.get("password") != request.form.get(
+             "confirm_password"):
+                flash("Passwords don't match")
+                return redirect(url_for("register"))
+
+        register = {
+            "type-of-help": request.form.get("type_of_help"),
+            "email": request.form.get("email"),
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
