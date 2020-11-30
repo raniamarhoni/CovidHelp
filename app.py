1aeaadea6e196587629c0d96fc2710db085cc864
raniamarhoni <marhoni@outlook.com>
2020-11-30 13:43:39 +0000

    home buttons styles hover added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-import datetime 
+from datetime import datetime, timedelta
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
